5374eb5828debc2ea507b06957eb2e003c025266
Jens Nordström <grinspooner@Jenss-MacBook-Pro.local>
2026-02-19 15:32:40 +0100

    Optimize credentials

--- JavaScript/test-login.js
+++ JavaScript/test-login.js
@@ -18,26 +18,24 @@ async function login() {
 
   await driver.sleep(2000);
 
-  // Set credentials
-  let credentials_passed = [
-    { username_passed: 'visual_user', password_passed: 'secret_sauce' }
-  ];
-
-  let credentials_failed = [
-    { username_failed: 'no_login', password_failed: 'no_login' }
+  // Setup credentials
+  const credentials = [
+    { username: 'visual_user', password: 'secret_sauce', type: 'passed' },
+    { username: 'wrong_user', password: 'no_secret_sauce', type: 'failed' }
   ];
 
   // Automate successful login
-  for (const selected of credentials_passed) {
-    let usernameField = await driver.findElement({ xpath: '//*[@id="user-name"]' });
-    await usernameField.sendKeys(selected.username_passed);
+  const userPassed = credentials.find(item => item.type === 'passed');
 
-    let passwordField = await driver.findElement({ xpath: '//*[@id="password"]' });
-    await passwordField.sendKeys(selected.password_passed);
+  let usernameField = await driver.findElement({ xpath: '//*[@id="user-name"]' });
+  await usernameField.sendKeys(userPassed.username);
 
-    let loginButton = await driver.findElement({ xpath: '//*[@id="login-button"]' });
-    await loginButton.click();
-  }
+  let passwordField = await driver.findElement({ xpath: '//*[@id="password"]' });
+  await passwordField.sendKeys(userPassed.password);
+
+  let loginButton = await driver.findElement({ xpath: '//*[@id="login-button"]' });
+  await loginButton.click();
+
 
   // Validate successful login
   const inventoryHeader = await driver.findElement({ xpath: '//*[@id="header_container"]/div[2]/span' });
@@ -63,20 +61,20 @@ async function login() {
   await driver.sleep(2000);
 
   // Automate failed login
-  for (const selected of credentials_failed) {
-    usernameField = await driver.findElement({ xpath: '//*[@id="user-name"]' });
-    await usernameField.sendKeys(selected.username_failed);
+  const userFailed = credentials.find(item => item.type === 'failed');
 
-    passwordField = await driver.findElement({ xpath: '//*[@id="password"]' });
-    await passwordField.sendKeys(selected.username_failed);
+  usernameField = await driver.findElement({ xpath: '//*[@id="user-name"]' });
+  await usernameField.sendKeys(userFailed.username);
 
-    loginButton = await driver.findElement({ xpath: '//*[@id="login-button"]' });
-    await loginButton.click();
+  passwordField = await driver.findElement({ xpath: '//*[@id="password"]' });
+  await passwordField.sendKeys(userFailed.username);
 
-    // Validate failed login
-    if (inventoryHeader) {
-      console.log('- Failed: Login');
-    }
+  loginButton = await driver.findElement({ xpath: '//*[@id="login-button"]' });
+  await loginButton.click();
+
+  // Validate failed login
+  if (inventoryHeader) {
+    console.log('- Failed: Login');
   }
 
   await driver.sleep(2000);
