e7d357738283e6a0aac314c0301a7140a0d2e10e
Jens Nordström <jens.nordstrom@gmail.com>
2026-03-14 19:51:36 +0100

    Update tests

--- JavaScript/get-request.js
+++ JavaScript/get-request.js
@@ -30,23 +30,23 @@ describe('API functionality on fakestoreapi.com/products', function () {
 
       xhr.onload = function () {
         try {
-          // TEST 1: Validate GET Response
-          assert.strictEqual(xhr.status, 200, `Expected status 200, got ${xhr.status}`);
+          // TEST 1: Validate GET Response status
+          assert.strictEqual(xhr.status, 200, `Expected status 200, instead got: ${xhr.status}`);
 
           // Setup data to be used for TEST 2-4
           let API_response = JSON.parse(xhr.responseText);
           const API_id15 = API_response.find(item => item && item.id === 15);
           const mockData_id15 = mockData.find(item => item && item.id === 15);
 
-          // TEST 2: Validate number of total ID's (Products) and compare with mock data
+          // TEST 2: Validate number of total ID's (Products) and compare with the mock data and API response
           const idCount = API_response.filter(item => item).length;
-          assert.strictEqual(mockData.length, idCount, `Mismatch in product count: mockData(${mockData.length}) vs API(${idCount})`);
+          assert.strictEqual(mockData.length, idCount, `Mismatch in product count: mock data: (${mockData.length}) API response: (${idCount})`);
 
-          // TEST 3: Validate items and content of product with ID 15 and compare with mock data
-          assert.ok(API_id15 && mockData_id15, "Product with id 15 should exist in both API and mock data");
-          assert.strictEqual(Object.keys(API_id15).length, 7, "API product with id 15 should have 7 properties");
+          // TEST 3: Validate object for ID 15 and compare with the mock data and API response
+          assert.ok(API_id15 && mockData_id15, "The object for ID 15 should be equal in the mock data and API response");
+          assert.strictEqual(Object.keys(API_id15).length, 7, "ID 15 should have 7 product properties");
 
-          // TEST 4: Validate correct fields and data for ID 15
+          // TEST 4: Validate correct fields and it's data for ID 15 against the mock data and API response
           const levelOneKeys = ['id', 'title', 'price', 'category', 'description', 'image'];
           for (const key of levelOneKeys) {
             assert.strictEqual(
